485978cba15659eacb8b4b530584a11b19a54355
Thato Semoko <thatosmk@gmail.com>
2017-12-05 17:04:56 +0200

    added earth program, struggling to form west word

---
+++ puzzles/earth.py
@@ -0,0 +1,59 @@
+import random
+
+def build_dict():
+    # build random words from the letters given in a list
+    # the letters are associalted with them, a digit,
+    
+    letters = ['A','E','H','N','O','R','S','T','U', 'W']
+
+    # build a dictionary assigning random digits to letters
+    # ensure that letters[keys] have different digits[keys]
+    earth_dict = {}
+
+    # get a sample of random digits
+    digits = random.sample(xrange(0,9), 9)
+
+    # associate letter to digit in a dictionary
+    earth_dict = dict(zip(letters, digits))
+
+    return  earth_dict
+
+def build_words(earth_dict):
+    # build a list of words
+    words = []
+    word = ""
+
+    # list of valid words
+    valid_words = ['EARTH','EAST', 'NORTH', 'SOUTH','WEST']
+
+    # list of earth_dict keys
+    letters = earth_dict.keys()
+    letters.append("")
+
+    # iterate to find a word 
+    # then add the word to words
+    # but you must ensure that all the words are different
+    while(len(words) < 5):
+
+        for i in range(5):
+            # randomise the indices of letters going to word
+            # add letter to word
+            word += letters[random.randint(0,9)]
+
+
+        if word.startswith("W"):
+            print word
+        # check if a word is valid
+        if word in valid_words:
+            words.append(word)
+            valid_words.remove(word)
+        elif word[:4] in valid_words:
+            words.append(word[:4])
+            valid_words.remove(word[:4])
+        else:
+            word = ""
+       
+        print words
+    print words
+
+build_words(build_dict())
